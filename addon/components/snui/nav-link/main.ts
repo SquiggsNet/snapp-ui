13120e953faa66eba89f0b41ec2b5d1f91c13e83
Scott <squiggs.rafael@gmail.com>
2024-10-22 19:37:43 -0300

    wip: lean a bit more into themes adding references to primary color variants

--- addon/components/snui/nav-link/main.ts
+++ addon/components/snui/nav-link/main.ts
@@ -3,8 +3,8 @@ import SnuiNavLinkBaseComponent from './base';
 export default class SnuiNavLinkMainComponent extends SnuiNavLinkBaseComponent {
   get classes(): string {
     const baseClasses = 'rounded-md px-3 py-2 text-sm font-medium';
-    const activeClasses = 'bg-gray-900 text-white';
-    const inactiveClasses = 'text-gray-300 hover:bg-gray-700 hover:text-white';
+    const activeClasses = 'bg-primary-900 text-white';
+    const inactiveClasses = 'text-gray-300 hover:bg-primary-700 hover:text-white';
     return `${baseClasses} ${this.args.isCurrent ? activeClasses : inactiveClasses} ${this.args.class || ''}`;
   }
 }
